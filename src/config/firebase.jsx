f74fc2cd2f75ad065f7fbfdace5cf562470a54f6
david <davicastiblanco5@gmail.com>
2026-04-23 13:45:40 -0500

    Volver a claves fijas en Firebase config

--- src/config/firebase.jsx
+++ src/config/firebase.jsx
@@ -4,12 +4,13 @@ import { getAuth } from 'firebase/auth'
 
 // Tu configuración de Firebase (USANDO TUS VALORES REALES)
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAVNVucDIrRv-ugVEowQVw1CRJsHo5OM_M",
+  authDomain: "nail-beauty-spa.firebaseapp.com",
+  projectId: "nail-beauty-spa",
+  storageBucket: "nail-beauty-spa.firebasestorage.app",
+  messagingSenderId: "903615920209",
+  appId: "1:903615920209:web:764be88a630c8b788ba070",
+  measurementId: "G-96M5QQBYJM"
 }
 
 // Inicializar Firebase
